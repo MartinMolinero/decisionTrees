fc15c32a644810c4c52a4854efb3e786e1afaefd
MartinMolinero <a01204818@itesm.mx>
2018-03-21 15:29:21 -0600

    parsing ready

--- id3.rb
+++ id3.rb
@@ -1,7 +1,8 @@
 class Attribute
 
-  def initialize
-    @values = Hash.new
+  def initialize(name, values)
+    @name = name
+    @values = values.clone
     @total_values = 0
     @array_match_values = Hash.new
     @depth = -1
@@ -30,8 +31,52 @@ class Attribute
 end
 
 
+def get_attributes(total_input)
+  attributes = Array.new
+  total_input.each do |line|
+
+    if line.match(/@attribute\s.*/)
+      attr_second = line.split(/@attribute\s(.*)\s/)
+      attr_second = attr_second.join("")
+      attr_second = attr_second.split(/\s/)
+      name = attr_second[0]
+      values = attr_second[1..-1].join("")
+      values = values.gsub(/[{|}]/,'')
+      values = values.split(",")
+      a = Attribute.new(name, values)
+      attributes.push(a)
+    end
+  end
+  attributes
+end
+
+def get_data(total_input)
+  data = Array.new
+  read = false
+  total_input.each do |line|
+    trueread = line.match(/@data(.)*/)
+    if read and line[0] != '%'
+      line = line.split(',')
+      data.push(line)
+    end
+
+    if trueread
+      read = true
+    end
+  end
+  data
+end
+
+
+
 def main
-  puts "hello world"
+  #parse attributes
+  input = $stdin.readlines
+  attributes = get_attributes(input)
+  data = get_data(input)
+  puts "data #{data} attributes #{attributes}"
+
+
 end
 
 main
