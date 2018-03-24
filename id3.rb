75154991321b767e58c4946cb936331804dfa327
MartinMolinero <a01204818@itesm.mx>
2018-03-23 20:11:40 -0600

    initial entropy

--- id3.rb
+++ id3.rb
@@ -54,6 +54,7 @@ def get_data(total_input)
   data = Array.new
   read = false
   total_input.each do |line|
+    line = line.chomp
     trueread = line.match(/@data(.)*/)
     if read and line[0] != '%'
       line = line.split(',')
@@ -68,15 +69,43 @@ def get_data(total_input)
 end
 
 
+def arr_to_hash(arr, hash)
+  arr.each do |value|
+    hash.merge!({ value => 0 } )
+  end
+end
+
+def get_initial_entropy(attributes, data)
+  counter  = {}
+  arr_to_hash(attributes, counter)
+  puts "#{counter}"
+  size = data.size
+  data.each do |row|
+    val = row.last.to_s
+    counter[val] = counter[val]+1
+  end
+  result = 0
+  counter.each do |key, value|
+    proportion = value.to_f / size.to_f
+    neg_prop = proportion * -1
+    puts "#{neg_prop}"
+    result += neg_prop * Math.log2(proportion)
+  end
+end
+
+
+
 
 def main
   #parse attributes
   input = $stdin.readlines
   attributes = get_attributes(input)
   data = get_data(input)
-  puts "data #{data} attributes #{attributes}"
-
-
+  last_attribute = attributes.last
+  last_attribute_values = last_attribute.values
+  puts "data #{data} attributes #{attributes} \n last_attribute  #{last_attribute.inspect} values #{last_attribute_values}"
+  h = get_initial_entropy(last_attribute_values, data)
+  split(attributes)
 end
 
 main
