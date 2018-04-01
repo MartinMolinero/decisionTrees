abb9170d8f33f2c1666921930b91fc490e762995
MartinMolinero <a01204818@itesm.mx>
2018-04-01 15:58:04 -0500

    WIP and debugging

--- id3.rb
+++ id3.rb
@@ -4,16 +4,23 @@ class Attribute
     @name = name
     @values = values.clone
     @total_values = 0
-    @array_match_values = Hash.new
-    @depth = -1
+    @hash_filtered_data = Hash.new
+    @hash_count_values = Hash.new
   end
 
+  def name
+    @name
+  end
   def total_values
     @total_values
   end
 
-  def array_match_values
-    @array_match_values
+  def hash_filtered_data
+    @hash_filtered_data
+  end
+
+  def hash_count_values
+    @hash_count_values
   end
 
   def values
@@ -24,10 +31,25 @@ class Attribute
     @depth = depth
   end
 
-  def depth
-    @depth
+
+  def filter_data(attribute_index, values, data)
+    @hash_filtered_data = Hash.new {|h,k| h[k]=[]}
+    values.each do |v|
+      data.each do |d|
+        if d[attribute_index] == v
+          @hash_filtered_data[v] << d
+        end
+      end
+    end
+    count_data()
   end
 
+  def count_data()
+    @hash_count_values = Hash.new
+    @hash_filtered_data.each do |k,v|
+      @hash_count_values.merge!({k => v.length})
+    end
+  end
 end
 
 
@@ -50,6 +72,16 @@ def get_attributes(total_input)
   attributes
 end
 
+def reduce_rows(data, attribute_index, value)
+  filtered = []
+  data.each do |row|
+    if(row[attribute_index] == value)
+      filtered.push(row)
+    end
+  end
+  return filtered
+end
+
 def get_data(total_input)
   data = Array.new
   read = false
@@ -60,7 +92,6 @@ def get_data(total_input)
       line = line.split(',')
       data.push(line)
     end
-
     if trueread
       read = true
     end
@@ -68,32 +99,73 @@ def get_data(total_input)
   data
 end
 
-
 def arr_to_hash(arr, hash)
   arr.each do |value|
     hash.merge!({ value => 0 } )
   end
 end
 
-def get_initial_entropy(attributes, data)
+def get_entropy(attributes, data)
   counter  = {}
+  puts "input entropy data #{data}"
   arr_to_hash(attributes, counter)
-  puts "#{counter}"
   size = data.size
   data.each do |row|
     val = row.last.to_s
     counter[val] = counter[val]+1
   end
+  puts "counter #{counter}"
   result = 0
   counter.each do |key, value|
-    proportion = value.to_f / size.to_f
-    neg_prop = proportion * -1
-    puts "#{neg_prop}"
-    result += neg_prop * Math.log2(proportion)
+    if (counter[key] > 0)
+      proportion = value.to_f / size.to_f
+      neg_prop = proportion * -1
+      result += neg_prop * Math.log2(proportion)
+    end
   end
+  puts "result #{result}"
+  return result
+end
+
+def information_gain(data, h_total_entropy, attribute, attribute_index, last_attribute_values)
+  result = 0
+  attribute.values.each do |a_v|
+    filtered_data = reduce_rows(data, attribute_index, a_v)
+    puts "filtered for #{a_v} in space #{attribute_index} \ndata: #{filtered_data}"
+    aux = get_entropy(last_attribute_values, filtered_data)
+    result -= (aux * (filtered_data.length / data.length))
+    puts "formula = #{aux} * #{filtered_data.length} / #{data.length} "
+  end
+  puts "info gain #{h_total_entropy} - #{result}"
+  return h_total_entropy + result
+
 end
 
 
+def split(attributes, data, last_attribute_values, h_total_entropy, visited, depth)
+  max = 0
+  chosen_position = -1
+  attributes.each_with_index do |a, i|
+    if(!visited.include?(i))
+      aux = information_gain(data, h_total_entropy, a, i, last_attribute_values)
+      if(aux > max)
+        max = aux
+        chosen_position = i
+      end
+    end
+  end
+  result = attributes[chosen_position]
+  result.values.each do |v|
+    filtered = reduce_rows(data, chosen_position, v)
+    puts "chosing #{result.name} value: #{v}"
+    if(get_entropy(last_attribute_values, filtered) == 0)
+      puts "answer: #{result.name} value #{v}"
+    else
+      visited.push(result)
+      split(attributes, filtered, last_attribute_values, h_total_entropy, visited, depth+1)
+    end
+  end
+end
 
 
 def main
@@ -102,10 +174,14 @@ def main
   attributes = get_attributes(input)
   data = get_data(input)
   last_attribute = attributes.last
+  attribute_index = attributes.index(last_attribute)
   last_attribute_values = last_attribute.values
-  puts "data #{data} attributes #{attributes} \n last_attribute  #{last_attribute.inspect} values #{last_attribute_values}"
-  h = get_initial_entropy(last_attribute_values, data)
-  split(attributes)
+  last_attribute.filter_data(attribute_index, last_attribute_values, data)
+  h_total_entropy = get_entropy(last_attribute_values, data)
+  depth = 0
+  visited = []
+  split(attributes, data, last_attribute_values, h_total_entropy, visited, depth)
+  puts "last attribute #{last_attribute.hash_filtered_data}"
 end
 
 main
